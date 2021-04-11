bd1c0220e7ffb1f7ec538077ccee4af6c1adbc0c
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-04-11 13:31:52 -0500

    function added to change bg color per hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,32 @@
-// add current day to the header
-function currentDay() {
-  const currentDayId = document.getElementById("currentDay");
-  const m = moment();
+const descriptions = document.getElementsByClassName("description");
+let currentHour = parseInt(moment().format("H"));
 
-  currentDayId.textContent = m.format("dddd, MMMM Mo YYYY");
+Array.from(descriptions).forEach((description) => {
+  let descriptionIdString = description.id,
+    descriptionHour;
+  if (descriptionIdString) {
+    descriptionHour = parseInt(descriptionIdString);
+  }
+  if (descriptionHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === descriptionHour) {
+      setColor(description, "red");
+    } else if (
+      currentHour < descriptionHour &&
+      currentHour > descriptionHour - 6
+    ) {
+      setColor(description, "green");
+    } else if (
+      currentHour > descriptionHour &&
+      currentHour < descriptionHour + 6
+    ) {
+      setColor(description, "lightgrey");
+    } else {
+      setColor(description, "white");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
 }
-currentDay();
-
-// $(function () {
-//   //currentTime to determine if the hour matchs for color
-//   var currentHour = moment().format("H A");
-//   var tete = $("td.hour").text();
-//   console.log(currentHour);
-//   console.log(tete);
-
-//   if (currentHour === currentHour) {
-//     $(".description").css("background", "yellow");
-//   }
-// });
-
-// $(document).ready(function () {
-//   var currentHour = moment().format("H A");
-//   console.log(currentHour);
-
-//   // Loop through each div element with the class box
-//   $(".hour").each(function () {
-//     // Test if the div element is empty
-//     if ($(this === currentHour)) {
-//       $(this).css("background", "yellow");
-//     }
-//   });
-// });
-
-// loop over time blocks
-// var currentHour = moment().format("H A");
-
-var test = document.getElementsByClassName("hour");
-console.log(test);
