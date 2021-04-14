704b247bf6e81b407c92f6656726ad18b9462d53
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-04-13 20:09:41 -0500

    color bg fixed for timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
 const descriptions = document.getElementsByClassName("description");
 let currentHour = parseInt(moment().format("H"));
+let todoForm = document.querySelector(".todo-form");
+var btn = document.querySelectorAll(".saveBtn");
+
+// text array
+textArr = [];
+
+function addText(event) {
+  // localStorage();
+  event.preventDefault();
+
+  let textArea = document.querySelector("#text-area");
+
+  //get value from text area
+  var textAreaInput = textArea.value.trim();
+  console.log(textAreaInput);
+
+  //add new text to text array
+  textArr.push(textAreaInput);
+  console.log(textArr);
+}
+
+// function localStorage() {
+//   var btn = document.querySelector(".saveBtn");
+//   btn.addEventListener("click", addText);
+// }
 
 Array.from(descriptions).forEach((description) => {
   let descriptionIdString = description.id,
@@ -13,12 +38,12 @@ Array.from(descriptions).forEach((description) => {
       setColor(description, "red");
     } else if (
       currentHour < descriptionHour &&
-      currentHour > descriptionHour - 6
+      currentHour > descriptionHour - 12
     ) {
       setColor(description, "green");
     } else if (
       currentHour > descriptionHour &&
-      currentHour < descriptionHour + 6
+      currentHour < descriptionHour + 12
     ) {
       setColor(description, "lightgrey");
     } else {
@@ -30,3 +55,7 @@ Array.from(descriptions).forEach((description) => {
 function setColor(element, color) {
   element.style.backgroundColor = color;
 }
+
+// local storage
+
+todoForm.addEventListener("submit", addText);
